8a9563af54bbd24795b25ff5ef33fc1ecf9be625
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2026-01-12 16:38:24 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+// MathJax
+(function() {
+    const mathjaxURL = "https://cdn.jsdelivr.net/npm/mathjax@4.0.0/tex-mml-chtml.js";
 
+    if (!document.getElementById("MathJax-script")) {
+        const script = document.createElement("script");
+        script.id = "MathJax-script";
+        script.async = true;
+        script.src = mathjaxURL;
+
+        document.head.appendChild(script);
+    }
+})();
+
+// Drive pdf
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll("iframe.d").forEach(iframe => {
+        const id = iframe.textContent.trim();
+        iframe.textContent = "";
+        iframe.src = `https://drive.google.com/file/d/${id}/preview`;
+        iframe.setAttribute("allow", "autoplay");
+    });
+});
